aa0797bc410beab18036515ac61ac60f9d6e5eb9
Luciana Adriao <luicianaasantos@hotmail.com>
2022-10-12 22:12:33 -0300

    Initial Commit

---
+++ IndividualChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IndividualChallenge
+//
+//  Created by Luciana Adrião on 12/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
